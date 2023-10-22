59578824e3161ea3baf548f28b4e4af6277f07ab
Mihir Karbelkar <karbelkar.mihir@gmail.com>
2023-10-22 22:41:00 +0530

    fix vercel url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,10 @@
 import { cookies } from 'next/headers';
 
 export const checkEnvironment = () => {
-  let base_url = process.env.API_BASE_URL;
-  return process.env.VERCEL_URL || base_url || '';
+  let base_url = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : process.env.API_BASE_URL;
+  return base_url || '';
 };
 export const api = (input: string, init?: RequestInit | undefined) => {
   return fetch(checkEnvironment()?.concat(input), {
